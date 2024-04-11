9e50a3f0ee831bfa6dd6e7fe5aad9321164026af
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2024-04-11 12:24:17 +0200

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -42,7 +42,7 @@ return {
 
   -- change some telescope options and a keymap to browse plugin files
   {
-    "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope.nvim", tag = '0.1.4'
     keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
